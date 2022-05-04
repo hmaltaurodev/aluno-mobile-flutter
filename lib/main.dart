9bf73a58a0b09964c3f760f365c8030740494864
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-05-03 22:55:35 -0300

    Commit inicial.

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aluno_mobile_flutter/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Aluno Mobile',
+    home: LoginPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
